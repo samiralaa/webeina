f7fb8b8a2563237a7a023e6252d3dfad1e765e56
Samir Alaa <76245712+samiralaa@users.noreply.github.com>
2025-01-31 17:22:50 +0400

    Update header-services.blade.php

--- resources/views/components/header-services.blade.php
+++ resources/views/components/header-services.blade.php
@@ -17,58 +17,59 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 @if (app()->getLocale() === 'ar')
                 <ul class="navbar-nav me-auto" id="navbar">
-                @else
-                <ul class="navbar-nav ms-auto" id="navbar">
-                @endif
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{route('home')}}" >
-                            {{ __('messages.home') }}
-                        </a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="{{ route('user-profile') }}" id="navbarDropdown" role="button">
-                            {{ __('messages.services') }}
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            @foreach ($services as $service)
-                            <li>
-                                <a class="dropdown-item" href="{{ route('serves.details', ['id' => $service->id]) }}">
-                                    {{ $service->name[app()->getLocale()] }}
-                                </a>
-                            </li>
-                            @endforeach
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">{{ __('messages.partners') }}</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="companyDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            {{ __('messages.company') }}
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="companyDropdown">
-                            <li><a class="dropdown-item" href="{{ route('faqs.index') }}">{{ __('messages.faqs') }}</a></li>
-                            <li><a class="dropdown-item" href="{{route('about-us')}}">{{ __('messages.about_us') }}</a></li>
-                            <li><a class="dropdown-item" href="#">{{ __('messages.dubai_branch') }}</a></li>
-                            <li><a class="dropdown-item" href="#">{{ __('messages.cairo_branch') }}</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        @if (app()->getLocale() === 'ar')
-                        <a href="{{ route('language.change', ['locale' => 'en']) }}" class="nav-link lang">en</a>
-                        @else
-                        <a href="{{ route('language.change', ['locale' => 'ar']) }}" class="nav-link">ع</a>
+                    @else
+                    <ul class="navbar-nav ms-auto" id="navbar">
                         @endif
-                    </li>
-                    <li class="nav-item">
-                        <a href="{{route('contact')}}" class="request-quote-btn quote">
-                            <i class="fa-solid fa-arrow-right"></i>
-                            <span>{{ __('messages.get_in_touch') }}</span>
-                        </a>
-                    </li>
-                </ul>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('home')}}">
+                                {{ __('messages.home') }}
+
+                            </a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="{{ route('user-profile') }}" id="navbarDropdown" role="button">
+                                {{ __('messages.services') }}
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                @foreach ($services as $service)
+                                <li>
+                                    <a class="dropdown-item" href="{{ route('serves.details', ['id' => $service->id]) }}">
+                                        {{ $service->name[app()->getLocale()] }}
+                                    </a>
+                                </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">{{ __('messages.partners') }}</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="companyDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                {{ __('messages.company') }}
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="companyDropdown">
+                                <li><a class="dropdown-item" href="{{ route('faqs.index') }}">{{ __('messages.faqs') }}</a></li>
+                                <li><a class="dropdown-item" href="{{route('about-us')}}">{{ __('messages.about_us') }}</a></li>
+                                <li><a class="dropdown-item" href="#">{{ __('messages.dubai_branch') }}</a></li>
+                                <li><a class="dropdown-item" href="#">{{ __('messages.cairo_branch') }}</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            @if (app()->getLocale() === 'ar')
+                            <a href="{{ route('language.change', ['locale' => 'en']) }}" class="nav-link lang">en</a>
+                            @else
+                            <a href="{{ route('language.change', ['locale' => 'ar']) }}" class="nav-link">ع</a>
+                            @endif
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{route('contact')}}" class="request-quote-btn quote">
+                                <i class="fa-solid fa-arrow-right"></i>
+                                <span>{{ __('messages.get_in_touch') }}</span>
+                            </a>
+                        </li>
+                    </ul>
             </div>
     </nav>
 </header>
