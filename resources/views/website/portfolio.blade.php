13f51f5c1490825aa4ab9b2175ed7b350f2386bc
Mohamed Yasser <134814075+Moyasser05@users.noreply.github.com>
2025-02-05 13:07:26 +0200

    add new in portfolio

--- resources/views/website/portfolio.blade.php
+++ resources/views/website/portfolio.blade.php
@@ -13,7 +13,31 @@
     </div>
 </div>
 
+<!-- Projects -->
+<div class="container py-5">
+    <div class="row">
+        <div class="project-card col-6 d-flex flex-column">
+            <div class="project-img">
+                <img src="https://innowise.com/wp-content/uploads/2025/01/Small-Cover-Automated-environmental-data-collection_-25-reduction-in-certification-costs-Anya-Kh.-Dasha-Tr.png" alt="">
+            </div>
 
+            <div class="project-content p-4">
+                <h2 class="project-title">Automated environmental data collection</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus quo inventore eius ipsam itaque earum tempore voluptate nostrum dicta sed?</p>
+            </div>
+        </div>
+        <div class="project-card col-6 d-flex flex-column">
+            <div class="project-img">
+                <img src="https://innowise.com/wp-content/uploads/2025/01/Small-Cover-Automated-environmental-data-collection_-25-reduction-in-certification-costs-Anya-Kh.-Dasha-Tr.png" alt="">
+            </div>
+
+            <div class="project-content p-4">
+                <h2 class="project-title">Automated environmental data collection</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus quo inventore eius ipsam itaque earum tempore voluptate nostrum dicta sed?</p>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 
