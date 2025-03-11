bbd8d4af78d23011de07e0c836b3587197218743
Mohamed Yasser <134814075+Moyasser05@users.noreply.github.com>
2025-03-11 10:41:09 +0200

    add

--- resources/views/website/about-us.blade.php
+++ resources/views/website/about-us.blade.php
@@ -19,17 +19,12 @@
     <div class="about container">
         <div class="row align-items-center">
             <div class="col-lg-7">
-                <h2 class="fw-bold text-success mb-4">Our History</h2>
+                <h2 class="fw-bold text-success mb-4">{{ __('messages.our-history') }}</h2>
+                <p class="text-muted">{{ __('messages.p-history') }}</p>
                 <p class="text-muted">
-                Since our establishment in 2017 as a government sector ERP contractor, we have broadened our services to align with the evolving UAE digital landscape. This includes adding, refining, and customizing our digital solutions to meet the needs of a wide range of businesses and organizations. We enthusiastically embrace digital transformation and endeavor to be a leader in realizing the strategic digital vision of the Gulf region
-                </p>
+                 <span class="fw-bold">{{ __('messages.vision-statement') }}</span> <br>{{ __('messages.p-vision') }}</p>
                 <p class="text-muted">
-                 <span class="fw-bold">Vision Statement</span> <br>
-To be the undisputed leader in facilitating digital transformation within the region, setting the benchmark for innovation and excellence.
-</p>
-<p class="text-muted">
-                 <span class="fw-bold">Mission Statement</span> <br>
-                 We are committed to empowering businesses to reach their full potential by leveraging cutting-edge digital technologies and strategies.</p>
+                 <span class="fw-bold">{{ __('messages.mission-statement') }}</span> <br>{{ __('messages.p-mission') }}</p>
             </div>
             <div class="col-lg-5">
                 <img src="{{ asset('assets/images/who-we-are.png') }}" alt="Our Story" loading="lazy" class="img-fluid rounded">
@@ -42,34 +37,32 @@ To be the undisputed leader in facilitating digital transformation within the re
                 <img src="{{ asset('assets/images/why-choose-us.png') }}" alt="Our Story" loading="lazy" class="img-fluid rounded">
             </div>
             <div class="col-lg-7">
-                <h2 class="fw-bold text-success  mb-4">Our Mission Encompasses:</h2>
+                <h2 class="fw-bold text-success mb-4">{{ __('messages.mission-encompasses') }}</h2>
 
                 <p class="text-muted">
-                 <span class="fw-bold">Guiding and Supporting Businesses through Digital Transformation:</span> <br>
-                 We will act as a trusted partner, providing comprehensive guidance and support to businesses at every stage of their digital transformation journey.
-                 </p>
-
-                 <p class="text-muted">
-                 <span class="fw-bold">Crafting Tailored Digital Transformation Strategies:</span> <br>
-                 We recognize that each business is unique. Our team of experts will collaborate closely with clients to develop and implement customized digital transformation plans that align with their specific needs and objectives.
+                    <span class="fw-bold">{{ __('messages.guiding-supporting-title') }}</span> <br>
+                    {{ __('messages.guiding-supporting-text') }}
                 </p>
 
-                 <p class="text-muted">
-                 <span class="fw-bold">Driving Sustainable Growth:</span> <br>
-                 We are focused on delivering solutions that generate measurable and sustainable growth for our clients, enabling them to thrive in the digital age.
+                <p class="text-muted">
+                    <span class="fw-bold">{{ __('messages.tailored-strategies-title') }}</span> <br>
+                    {{ __('messages.tailored-strategies-text') }}
                 </p>
 
-                 <p class="text-muted">
-                 <span class="fw-bold">Collaborating with Diverse Organizations</span> <br>
-                 We are committed to working with a wide range of organizations, including both private and public sector entities, to drive digital transformation across all sectors of the economy.
-                 </p>
+                <p class="text-muted">
+                    <span class="fw-bold">{{ __('messages.sustainable-growth-title') }}</span> <br>
+                    {{ __('messages.sustainable-growth-text') }}
+                </p>
 
-                 <p class="text-muted">
-                 <span class="fw-bold">Maximizing the Benefits of Digital Transformation:</span> <br>
-                 Our goal is to ensure that our clients fully realize the transformative potential of digital technologies, achieving optimal outcomes and maximizing their return on investment.
-                 </p>
+                <p class="text-muted">
+                    <span class="fw-bold">{{ __('messages.collaborating-organizations-title') }}</span> <br>
+                    {{ __('messages.collaborating-organizations-text') }}
+                </p>
 
-
+                <p class="text-muted">
+                    <span class="fw-bold">{{ __('messages.maximizing-benefits-title') }}</span> <br>
+                    {{ __('messages.maximizing-benefits-text') }}
+                </p>
             </div>
         </div>
     </div>
--- resources/views/website/contact-us.blade.php
+++ resources/views/website/contact-us.blade.php
@@ -16,135 +16,66 @@
 
 <!-- Contact Us -->
 <section class="contact-section py-5 mt-5">
-
-
-
     <div class="container">
-        <h2 class="text-center mb-1">Get in Touch</h2>
-        <p class="text-center lead">We'd love to hear from you! Reach out to us with your questions or feedback.</p>
+        <h2 class="text-center mb-1">{{ __('messages.get-in-touch') }}</h2>
+        <p class="text-center lead">{{ __('messages.get-in-touch-text') }}</p>
 
-        <div class="row mt-5     d-flex justify-content-center">
-            {{-- <div class="col-12 col-md-6">
-                <img class="img-fluid" src="{{ asset('assets/images/contact-us.png') }}" alt="">
-        </div> --}}
+        <div class="row mt-5 d-flex justify-content-center">
+            <div class="col-12 col-md-7">
+                <h4 class="mb-5">{{ __('messages.send-message') }}</h4>
 
-        <div class="col-12 col-md-7">
-            <h4 class="mb-5">Send Us a Message</h4>
-
-            {{-- start form --}}
-
-            <form action="{{route('contact.store')}}" method="post">
-            @csrf
-                <div class="all col-12 d-md-flex" style="gap:50px">
-                    <div class="input col-sm-12 col-md-6">
-
-
-                        <div class=" mb-3 ">
-                            <div class="name mb-3">
-                                <label for="name" class="form-label text-capitalize fw-bold">First name</label>
-                                <input type="text" class="form-control" id="name" name="first_name" required>
-                            </div>
-                            <div class="last_name mb-3">
-                                <label for="email" class="form-label text-capitalize fw-bold">Last name</label>
-                                <input type="text" class="form-control" id="L-name" name="last_name" required>
-                            </div>
+                <form action="{{route('contact.store')}}" method="post">
+                    @csrf
+                    <div class="row g-3">
+                        <div class="col-md-6">
+                            <label for="first_name" class="form-label fw-bold">{{ __('messages.first-name') }}</label>
+                            <input type="text" class="form-control" id="first_name" name="first_name" required>
                         </div>
-
-
-                        <div class=" mb-3 ">
-                            <div class="last_name mb-3">
-                                <label for="email" class="form-label text-capitalize fw-bold">Business email</label>
-                                <input type="text" class="form-control" id="L-name" name="business_email" required>
-                            </div>
+                        <div class="col-md-6">
+                            <label for="last_name" class="form-label fw-bold">{{ __('messages.last-name') }}</label>
+                            <input type="text" class="form-control" id="last_name" name="last_name" required>
                         </div>
-                        <div class="name mb-3 ">
-                            <label for="name" class="form-label text-capitalize fw-bold">company</label>
-                            <input type="text" class="form-control" id="company-name" name="company" required>
+                        <div class="col-md-6">
+                            <label for="business_email" class="form-label fw-bold">{{ __('messages.business-email') }}</label>
+                            <input type="email" class="form-control" id="business_email" name="business_email" required>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="company" class="form-label fw-bold">{{ __('messages.company') }}</label>
+                            <input type="text" class="form-control" id="company" name="company" required>
                         </div>
                     </div>
-
-
-                    <div class="check col-sm-12 col-md-6">
-                        <div class="checkboxex d-flex flex-column pt-3 pt-md-0 pb-3">
-                            <label for="services" class="text-capitalize fw-bold">Choose services:</label>
-
-
-                            <div id="services" class="d-flex flex-column">
-                                @foreach ($serves as $ser )
-
+                    
+                    <div class="mt-3">
+                        <label class="fw-bold">{{ __('messages.choose-services') }}</label>
+                        <div class="d-flex flex-column">
+                            @foreach ($serves as $ser)
                                 <label>
-                                    <input type="checkbox" name="service_id" value="{{$ser->id}}">
-                                    {{$ser->name['en'] ?? "test"}}
+                                    <input type="checkbox" name="service_id[]" value="{{$ser->id}}"> {{$ser->name['en'] ?? "test"}}
                                 </label>
-                                @endforeach
-                            </div>
-
+                            @endforeach
                         </div>
                     </div>
-                </div>
-
-                <div class="mb-3">
-                    <label for="message" class="form-label text-capitalize fw-bold">About Your Project</label>
-                    <textarea class="form-control" id="message" name="message" rows="3" required placeholder="Tell us more about your project"></textarea>
-                </div>
-                <button type="submit" class="request-quote-btn quote" style="max-width: 200px;margin: 0">
-                    <span">Send Request</span>
-                </button>
-            </form>
+                    
+                    <div class="mt-3">
+                        <label for="message" class="form-label fw-bold">{{ __('messages.about-project') }}</label>
+                        <textarea class="form-control" id="message" name="message" rows="3" required placeholder="{{ __('messages.tell-us-more') }}"></textarea>
+                    </div>
+                    
+                    <button type="submit" class="btn btn-success mt-3">{{ __('messages.send-request') }}</button>
+                </form>
+            </div>
         </div>
     </div>
-    {{-- end form --}}
 
-
-    {{-- start Q-contact --}}
-
-    <div class="Q-contact pt-5 pb-5">
-
-        {{-- <div class="row mt-5 d-flex justify-content-center ">
-                <div class="col-12 col-md-6">
-                <img class="img-fluid" src="{{ asset('assets/images/contact-us.png') }}" alt="">
-    </div>
-
-    <div class="col-12 col-md-6">
-        <h4 class="text-center text-capitalize">quick contact</h4>
-        <form action="submit-form.php" method="post">
-            <div class="mb-3 pt-2 d-flex gap-3 ">
-                <div class="w-50">
-                    <label for="name" class="form-label text-capitalize fw-bold">Your Name</label>
-                    <input type="text" class="form-control" id="name" name="name" required>
-
-                </div>
-                <div class="mb-3 w-50">
-                    <label for="email" class="form-label text-capitalize fw-bold">Your Email</label>
-                    <input type="email" class="form-control" id="email" name="email" required>
-                </div>
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label text-capitalize fw-bold">Business email</label>
-                <input type="email" class="form-control" id="email" name="email" required>
-            </div>
-            <div class="mb-3">
-                <label for="message" class="form-label text-capitalize fw-bold">Message</label>
-                <textarea class="form-control" id="message" name="message" rows="3" required></textarea>
-            </div>
-            <button type="submit" class="btn btn-success">Send Message</button>
-        </form>
-    </div>
-    </div>
-
-    </div> --}}
-
-
-    {{-- end Q-contact --}}
     <div class="our-locations pt-5">
-        <h2 class="text-center">Our Locations</h2>
-        <div class="container" style="min-height: 500px; gap:80px ; ">
+        <h2 class="text-center">{{ __('messages.our-locations') }}</h2>
+        <div class="container d-flex flex-wrap justify-content-center" style="gap:80px; min-height: 500px;">
             <div class="location col-12 col-md-6">
-                <h5 class="mt-5 mb-2">Dubai Office:</h5>
+                <h5 class="mt-5 mb-2">{{ __('messages.dubai-office') }}</h5>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d6906.826275125326!2d31.356819099999996!3d30.053690099999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sar!2seg!4v1734935392128!5m2!1sar!2seg" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
             <div class="location col-12 col-md-6">
-                <h5 class="mt-5 mb-2">Cairo Office:</h5>
+                <h5 class="mt-5 mb-2">{{ __('messages.cairo-office') }}</h5>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3453.4117809543673!2d31.356859999999998!3d30.053729!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzDCsDAzJzEzLjQiTiAzMcKwMjEnMjQuNyJF!5e0!3m2!1sar!2seg!4v1737545059195!5m2!1sar!2seg" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
