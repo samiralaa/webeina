e68680ee8c105ff89b495a96baf6400665da4ff2
Mohamed Yasser <134814075+Moyasser05@users.noreply.github.com>
2025-03-25 13:04:24 +0200

    favicon

--- resources/views/website/layouts/main.blade.php
+++ resources/views/website/layouts/main.blade.php
@@ -8,7 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- #favicon -->
-    <link rel="shortcut icon" href="assets/images/l-01.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon" />
     <!-- #title -->
     <title>Webenia | Digital Solutions Agency</title>
     <!-- #keywords -->
